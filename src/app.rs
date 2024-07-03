98f8274d954e58f392250dbfa8a61fba3635ecb1
bmbp <zc_union@163.com>
2024-07-03 23:06:19 +0800

    feat: dialog

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,39 @@
+use crate::da::DayDialog;
 use eframe::App;
-use egui::{CentralPanel, Color32, Context};
-
-
-pub struct NoteDayApp {}
+use egui::{Button, CentralPanel, Color32, Context, Vec2};
+pub struct NoteDayApp {
+    dialog: DayDialog,
+    is_dialog_open: bool,
+}
 
 impl NoteDayApp {
     pub fn new(ctx: &eframe::CreationContext<'_>) -> Self {
         Self::set_app_fonts(ctx);
-        Self {}
+        NoteDayApp {
+            dialog: DayDialog::new(),
+            is_dialog_open: false,
+        }
     }
     fn set_app_fonts(ctx: &eframe::CreationContext<'_>) {
         // 加载自定义字体数据
-        let mut font_data = egui::FontData::from_static(include_bytes!("../fonts/SourceHanSerifCN-Regular.otf")); // 替换为你的字体文件路径
-        // 定义字体家族，关联字体数据
+        let mut font_data =
+            egui::FontData::from_static(include_bytes!("../fonts/SourceHanSerifCN-Regular.otf")); // 替换为你的字体文件路径
+                                                                                                  // 定义字体家族，关联字体数据
         let mut fonts = egui::FontDefinitions::default();
         fonts.font_data.insert("app_font".to_owned(), font_data);
         // 将自定义字体设为默认的中文字体
-        fonts.families.insert(egui::FontFamily::Monospace, vec!["app_font".to_owned()]);
-        fonts.families.insert(egui::FontFamily::Proportional, vec!["app_font".to_owned()]);
+        fonts
+            .families
+            .insert(egui::FontFamily::Monospace, vec!["app_font".to_owned()]);
+        fonts
+            .families
+            .insert(egui::FontFamily::Proportional, vec!["app_font".to_owned()]);
         // 应用字体定义
         ctx.egui_ctx.set_fonts(fonts);
+    }
+    fn show_popwindow(&mut self, ctx: &Context) {
+        println!("ddddd");
+        self.dialog.dialog_logic(ctx);
     }
 }
 
@@ -36,29 +50,40 @@ impl App for NoteDayApp {
                     let _ = ui.button("信箱");
                 });
                 ui.separator();
-                egui::Frame::group(ui.style())
-                    .fill(ui.visuals().widgets.noninteractive.bg_fill) // 设置背景颜色
-                    .stroke(egui::Stroke::new(1.0, egui::Color32::BLACK)) // 设置边框颜色和宽度
-                    .show(ui, |ui| {
-                        // 在Frame内部添加Label
-                        ui.add(egui::Label::new(egui::RichText::new("这是一个带有边框的Label")));
-                    });
-                egui::Frame::group(ui.style())
-                    .fill(ui.visuals().widgets.noninteractive.bg_fill) // 设置背景颜色
-                    .stroke(egui::Stroke::new(1.0, egui::Color32::BLACK)) // 设置边框颜色和宽度
-                    .show(ui, |ui| {
-                        // 在Frame内部添加Label
-                        ui.add(egui::Label::new(egui::RichText::new("这是一个带有边框的Label")));
-                    });
-                egui::Frame::group(ui.style())
-                    .fill(ui.visuals().widgets.noninteractive.bg_fill) // 设置背景颜色
-                    .stroke(egui::Stroke::new(1.0, egui::Color32::BLACK)) // 设置边框颜色和宽度
-                    .show(ui, |ui| {
-                        // 在Frame内部添加Label
-                        ui.add(egui::Label::new(egui::RichText::new("这是一个带有边框的Label")));
-                    });
+                egui::ScrollArea::both().show(ui, |ui| {
+                    egui::Grid::new("0001")
+                        .num_columns(5)
+                        .spacing(Vec2::new(2.0, 2.0))
+                        .show(ui, |ui| {
+                            for row in 0..5 {
+                                for item in 0..5 {
+                                    egui::Frame::group(ui.style())
+                                        .fill(ui.visuals().widgets.noninteractive.bg_fill) // 设置背景颜色
+                                        .stroke(egui::Stroke::new(1.0, egui::Color32::BLACK)) // 设置边框颜色和宽度
+                                        .show(ui, |ui| {
+                                            // 在Frame内部添加Label
+                                            ui.add(egui::Label::new(egui::RichText::new(format!(
+                                                "这是一个带有边框的{}",
+                                                item
+                                            ))));
+
+                                            if (ui.button("text").clicked()) {
+                                                if (self.dialog.show_dialog) {
+                                                    self.dialog.show_dialog = false;
+                                                }
+                                                self.dialog.text = format!("{}-{}", row, item);
+                                                self.dialog.show_dialog = true;
+                                            }
+                                            if (self.dialog.show_dialog) {
+                                                self.show_popwindow(ctx);
+                                            }
+                                        });
+                                }
+                                ui.end_row();
+                            }
+                        });
+                });
             });
         });
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod app;
 mod calcander;
+mod da;
 mod fourarea;
 mod inbox;
 
-use eframe::App;
-use crate::app::NoteDayApp;
+use app::NoteDayApp;
+use da::DayDialog;
 
 fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions { ..Default::default() };
-    eframe::run_native("日历记事本", options,
-                       Box::new(|cc| Box::new(NoteDayApp::new(cc))),
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
+    let mut app_dialog = DayDialog::new();
+    eframe::run_native(
+        "日历记事本",
+        options,
+        Box::new(|cc| Box::new(NoteDayApp::new(cc))),
     )
 }
